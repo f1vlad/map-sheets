fe530bf9e69554e6e37cdac5d051bc48eb69484f
f1vlad <kulchitski@gmail.com>
2018-01-19 19:12:46 -0800

    Add geocoder

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var data, request, store, storeFront, storeId;
+var data, request, store, storeFront, storeId, address, lat, lon;
 
 store = "https://spreadsheets.google.com/feeds/list/1Ouip-u1ZkHyJSqeinoJf-HXcKc08MjIodbnt1KLSF1A/od6/public/values?alt=json";
 storeFront = "https://docs.google.com/spreadsheets/d/1Ouip-u1ZkHyJSqeinoJf-HXcKc08MjIodbnt1KLSF1A/edit?usp=sharing";
@@ -23,9 +23,20 @@ request.send();
 
 setTimeout(function(){
   // console.log(data.feed.entry);
+  var geocoder = new google.maps.Geocoder();
+
   var i = 0;
+
   while(i < data.feed.entry.length) {
     console.log(data.feed.entry[i]['gsx$address']['$t'] + ' --- ' + data.feed.entry[i]['gsx$name']['$t']);
+    address = data.feed.entry[i]['gsx$address']['$t'];
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        lat = results[0].geometry.location.lat();
+        lon = results[0].geometry.location.lng();
+        console.log(lat + ', ' + lon);
+      }
+    });
     i++;
   }
 }, 3000)
