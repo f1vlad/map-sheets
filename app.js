d63688aa2c3ad332404d256e1f3f2cb1f09cc313
f1vlad <kulchitski@gmail.com>
2018-01-19 19:32:16 -0800

    Dynamically add markers

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var data, request, store, storeFront, storeId, address, lat, lon;
+var data, request, store, storeFront, storeId, address, lat, lon, marker, markers = [];
 
-store = "https://spreadsheets.google.com/feeds/list/1Ouip-u1ZkHyJSqeinoJf-HXcKc08MjIodbnt1KLSF1A/od6/public/values?alt=json";
+storeJson = "https://spreadsheets.google.com/feeds/list/1Ouip-u1ZkHyJSqeinoJf-HXcKc08MjIodbnt1KLSF1A/od6/public/values?alt=json";
 storeFront = "https://docs.google.com/spreadsheets/d/1Ouip-u1ZkHyJSqeinoJf-HXcKc08MjIodbnt1KLSF1A/edit?usp=sharing";
 storeId = "1Ouip-u1ZkHyJSqeinoJf-HXcKc08MjIodbnt1KLSF1A";
 request = new XMLHttpRequest();
-request.open('GET', store, true);
+request.open('GET', storeJson, true);
 
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
@@ -28,38 +28,34 @@ setTimeout(function(){
   var i = 0;
 
   while(i < data.feed.entry.length) {
-    console.log(data.feed.entry[i]['gsx$address']['$t'] + ' --- ' + data.feed.entry[i]['gsx$name']['$t']);
     address = data.feed.entry[i]['gsx$address']['$t'];
     geocoder.geocode({'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         lat = results[0].geometry.location.lat();
         lon = results[0].geometry.location.lng();
-        console.log(lat + ', ' + lon);
+        markers.push({lat: lat, lng: lon});
       }
     });
     i++;
   }
-}, 3000)
+}, 2000)
 
 function initMap () {
   setTimeout(function(){
     drawMap();
-  }, 3000);
+  }, 4000);
 }
 
 function drawMap() {
-  var uluru = {lat: -25.363, lng: 131.044},
-      huiluru = {lat: -25.033, lng: 130.1};
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -25.360, lng: 131.0},
-    zoom: 6
+    center: {lat: 37.3236421, lng: -102.01350230000003},
+    zoom: 4
   });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-  marker = new google.maps.Marker({
-    position: huiluru,
-    map: map
+
+  markers.forEach(function(ll) {
+    marker = new google.maps.Marker({
+      position: ll,
+      map: map
+    });
   });
 }
